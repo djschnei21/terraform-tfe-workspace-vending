3c4f14a93b53282cb4a1639f84c770596166ae4b
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 14:29:56 +0000

    allow sub modules to dictate provider versions

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    tfe = {
-      version = "~> 0.38.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
 }
 
 variable "app_ids" {
