bd555bd77b374a775b9544363868ad3e2a00a8fd
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 13:17:19 +0000

    connect each workspace to the correct repo/branch

--- modules/workspace/workspace.tf
+++ modules/workspace/workspace.tf
@@ -23,10 +23,21 @@ variable "app_envs" {
   default = []
 }
 
+data "tfe_oauth_client" "client" {
+  organization = "djs-tfcb"
+  name         = "github-test"
+}
+
 resource "tfe_workspace" "workspaces" {
   for_each = var.app_envs
 
   name         = "app-${var.app_id}-${each.key}"
   organization = "djs-tfcb"
   tag_names    = ["vending-machine-demo"]
+
+  vcs_repo {
+    identifier = "djschnei21/app-${var.app_id}"
+    branch = each.key
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
 }
