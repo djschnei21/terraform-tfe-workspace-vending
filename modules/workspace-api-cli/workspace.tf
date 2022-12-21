6344730188242e4697e5fa5a0e440ee8382f21d1
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-21 12:28:27 -0500

    new reps/ws names

--- modules/workspace-api-cli/workspace.tf
+++ modules/workspace-api-cli/workspace.tf
@@ -25,34 +25,31 @@ data "tfe_organization" "foo" {
   name = var.tf_org
 }
 
-# resource "tfe_project" "project" {
-#   organization = var.tf_org
-#   name = var.app_id
-# }
+variable "projects" {
+  type = bool
+  default = false
+}
 
-resource "tfe_workspace" "network_workspaces" {
+resource "tfe_project" "project" {
+  count = var.projects == true ? 1 : 0 
+  organization = var.tf_org
+  name = var.app_id
+}
+
+resource "tfe_workspace" "lz_workspaces" {
   for_each = var.app_envs
 
-  name         = "${var.app_id}-${each.key}-network"
-  organization = data.tfe_organization.foo.name
-  # project_id   = tfe_project.project.id
+  name         = "${each.key}-${var.app_id}-lz"
+  organization = var.tf_org
+  project_id   = var.projects == true ? tfe_project.project.id : null
   tag_names    = [var.app_id, each.key]
 }
 
-resource "tfe_workspace" "compute_workspaces" {
+resource "tfe_workspace" "app_workspaces" {
   for_each = var.app_envs
 
-  name         = "${var.app_id}-${each.key}-compute"
-  organization = data.tfe_organization.foo.name
-  # project_id   = tfe_project.project.id
-  tag_names    = [var.app_id, each.key]
-}
-
-resource "tfe_workspace" "storage_workspaces" {
-  for_each = var.app_envs
-
-  name         = "${var.app_id}-${each.key}-storage"
-  organization = data.tfe_organization.foo.name
-  # project_id   = tfe_project.project.id
+  name         = "${each.key}-${var.app_id}-app"
+  organization = var.tf_org
+  project_id   = var.projects == true ? tfe_project.project.id : null
   tag_names    = [var.app_id, each.key]
 }
