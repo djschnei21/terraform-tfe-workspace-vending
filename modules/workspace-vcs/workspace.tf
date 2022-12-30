b0519413b149173f59ddaabaae764620e167929a
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-30 12:46:36 -0500

    bracket star

--- modules/workspace-vcs/workspace.tf
+++ modules/workspace-vcs/workspace.tf
@@ -81,7 +81,7 @@ resource "tfe_workspace" "app_workspaces" {
 
 output "workspaces" {
   value = setunion(
-    toset(tfe_workspace.app_workspaces.*.name), 
-    toset(tfe_workspace.lz_workspaces.*.name)
+    toset(tfe_workspace.app_workspaces[*].name), 
+    toset(tfe_workspace.lz_workspaces[*].name)
     )
 }
